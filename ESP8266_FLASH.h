dd144aa929b0dbfd736a968206a958a58fad4af2
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-02 21:12:54 +0530

    added flash store location define

--- ESP8266_FLASH.h
+++ ESP8266_FLASH.h
@@ -13,6 +13,18 @@
 #include <ets_sys.h>
 #include <spi_flash.h>
 
+//////////////////////////////////
+//DEFINES
+//////////////////////////////////
+//FLASH SECTOR (4KB) TO STORE PROGRAM DATA
+//ESP_INIT_DATA STARTS AT 0X3FC AS PER
+//http://www.espressif.com/en/support/explore/get-started/esp8266/getting-started-guide#
+/*http://www.esp8266.com/viewtopic.php?f=34&t=2662*/
+//STORE PROGRAM INFO 1 SECTOR BEFORE THAT
+//DO NOT CHANGE THIS ADDRESS !
+//VALID FOR 4M FLASH CHIP WITH 1024+1024 MAP
+#define FLASH_STORE_SECTOR  0x3FE
+
 SpiFlashOpResult ESP8266_FLASH_erase(uint16_t sector_address);
 SpiFlashOpResult ESP8266_FLASH_write(uint16_t sector_address, uint32_t* source_address, uint32_t size);
 SpiFlashOpResult ESP8266_FLASH_read(uint16_t sector_address, uint32_t* destination_address, uint32_t size);
