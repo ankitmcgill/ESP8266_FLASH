81f938ecf2c89c81ad31a69fafaaa47abde9cb64
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-02 14:18:06 +0530

    added erase, write & read flash functionality

--- ESP8266_FLASH.h
+++ ESP8266_FLASH.h
@@ -11,6 +11,10 @@
 
 #include "user_interface.h"
 #include <ets_sys.h>
+#include <spi_flash.h>
 
+SpiFlashOpResult ESP8266_FLASH_erase(uint16_t sector_address);
+SpiFlashOpResult ESP8266_FLASH_write(uint16_t sector_address, uint32_t* source_address, uint32_t size);
+SpiFlashOpResult ESP8266_FLASH_read(uint16_t sector_address, uint32_t* destination_address, uint32_t size);
 
 #endif
