0e45b479fbc58db41258c8fc922433d7da362500
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-11-02 12:58:32 +0530

    Added initial file skeleton

---
+++ ESP8266_FLASH.c
@@ -0,0 +1,10 @@
+/*************************************************
+* ESP8266 FLASH LIBRARY
+*
+* NOVEMBER 2 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+* ***********************************************/
+
+#include "ESP8266_FLASH.h"
+
---
+++ ESP8266_FLASH.h
@@ -0,0 +1,16 @@
+/*************************************************
+* ESP8266 FLASH LIBRARY
+*
+* NOVEMBER 2 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+* ***********************************************/
+
+#ifndef _ESP8266_FLASH_H_
+#define _ESP8266_FLASH_H_
+
+#include "user_interface.h"
+#include <ets_sys.h>
+
+
+#endif
