04508d0c77b6a6bad12f5b72f0cb4af145d5bb36
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-06-02 13:17:58 +0530

    modified to include debug printing, flash address / sector number verification and better coding. NOT TESTED !

--- ESP8266_FLASH.c
+++ ESP8266_FLASH.c
@@ -2,23 +2,168 @@
 * ESP8266 FLASH LIBRARY
 *
 * NOVEMBER 2 2016
+*
 * ANKIT BHATNAGAR
 * ANKIT.BHATNAGARINDIA@GMAIL.COM
-* ***********************************************/
+*
+* REFERENCES
+*   (1) ESP8266 SDK GETTING STARTED GUIDE v2.8
+*       SECTION 4.1.1
+*       http://espressif.com/sites/default/files/documentation/2a-esp8266-sdk_getting_started_guide_en.pdf
+*   (2) ESP8266 FLASH RW OPERATIONS
+*       http://www.espressif.com/sites/default/files/99a-sdk-espressif_iot_flash_rw_operation_en_v1.0_0.pdf
+*
+* MOST OF THE 12F MODULES USE FLASH CHIP ID : 0x1640E0 (BERG MICRO 32MBIT / 4MB FLASH)
+*
+* FLASH IS A BLOCK DEVICE AND WORKS ON SECTORS (4KB)
+* BEFORE WRITING TO AN ADDRESS YOU NEED TO ERASE IT. ERASING CAN ONLY BE DONE AT THE LEAST OF A 4 KB BLOCK
+*
+* NOTE : FOR FLASH OPERATIONS, ALL READ/WRITE OPERATIONS NEED TO BE 4 BYTE ALIGNED
+*
+* AS PER THE DOCUMENTS, MEMORY MAP IS AS FOLLOWING (NON FOTA)
+*                                                                                                     0x400000
+* +------------+------------------------+-------------------------------------------+----------+------+
+* + FLASH      | IROM0TEXT              |       THIS AREA IS VARIABLE (DEPNDING     | ESP INIT | BLANK|
+* +  .BIN      |      .BIN              |        ON THE SIZE OF IROM0TEXT.BIN &     |     DATA |  .BIN|
+* + (MAX 64KB) |(MAX 76KB)              |        CAN BE USED TO STORE USER DATA     |    (8KB) | (8KB)|
+* +------------+------------------------+-------------------------------------------+----------+------+
+* 0x00000      0x10000                                                              0x3FC000   0x3FE000
+************************************************/
 
 #include "ESP8266_FLASH.h"
 
-SpiFlashOpResult ESP8266_FLASH_erase(uint16_t sector_address)
+//LOCAL LIBRARY VARIABLES////////////////////////////////
+//DEBUG RELATED
+static uint8_t _esp8266_flash_debug;
+//END LOCAL LIBRARY VARIABLES/////////////////////////////
+
+void ICACHE_FLASH_ATTR ESP8266_FLASH_SetDebug(uint8_t debug_on)
 {
-	return spi_flash_erase_sector(sector_address);
+	//SET DEBUG PRINTF ON(1) OR OFF(0)
+
+	_esp8266_flash_debug = debug_on;
 }
 
-SpiFlashOpResult ESP8266_FLASH_write(uint32_t flash_address, uint32_t* source_address, uint32_t size)
+void ICACHE_FLASH_ATTR ESP8266_FLASH_EraseSector(uint16_t sector_number)
 {
-	return spi_flash_write(flash_address, source_address, size);
+    //ERASE THE SPECIFIED SECTOR NUMBER
+    
+    //CHECK FOR SECTOR VALIDITY
+    if(_esp8266_flash_check_address_validity(sector_number * 4096))
+    {
+        //VALID
+        spi_flash_erase_sector(sector_number);
+        if(_esp8266_flash_debug)
+        {
+            os_printf("ESP8266 : FLASH : Sector #%u erased\n", sector_number);
+        }
+        return;
+    }
+    //INVALID
+    if(_esp8266_flash_debug)
+    {
+        os_printf("ESP8266 : FLASH : Sector #%u invalid. Not erased\n", sector_number);
+    }
+    return;
 }
 
-SpiFlashOpResult ESP8266_FLASH_read(uint32_t flash_address, uint32_t* destination_address, uint32_t size)
+void ICACHE_FLASH_ATTR ESP8266_FLASH_EraseBlock(uint16_t sector_number, uint16_t num_blocks)
 {
-	return spi_flash_read(flash_address, destination_address, size);
+    //ERASE A CHAIN OF SECTORS STARTING AT THE SPECIFIED SECTOR
+    //VALIDITY OF A SECTOR ADDRESS WOULD BE DONE IN THE ERASE SECTOR SINGLE FUNCTION
+    
+    uint8_t i;
+    for(i = 0; i < num_blocks; i++)
+    {
+        ESP8266_FLASH_EraseSector(sector_number + i);
+    }
+    if(_esp8266_flash_debug)
+    {
+        os_printf("ESP8266 : FLASH : Sector chain erase of length %u starting at sector #%u done\n", num_blocks, sector_number);
+    }
+}
+
+void ICACHE_FLASH_ATTR ESP8266_FLASH_WriteAddress(uint32_t flash_address, uint32_t* source_address, uint32_t size)
+{
+    //COPY SPECIFIED NUMBER OF BYTES FROM THE SOURCE ADDRESS INTO THE FLASH ADDRESS
+    //NOTE FLASH NEEDS TO BE ERASED BEFORE IT CAN BE WRITTEN INTO, AND ERASE HAPPENS ONLY
+    //AT THE BLOCK LEVEL (4KB)
+    
+    //VERIFY FLASH ADDRESS
+    if(!_esp8266_flash_check_address_validity(flash_address))
+    {
+        if(_esp8266_flash_debug)
+        {
+            os_printf("ESP8266 : FLASH : Flash write. Starting flash address %x invalid\n", flash_address);
+        }
+        return;
+    }
+    
+    uint16_t starting_sector_num = (flash_address / 4096);
+    uint16_t num_blocks = (size / 4096);
+    
+    //ERASE BLOCKS
+    ESP8266_FLASH_EraseBlock(starting_sector_num, num_blocks);
+    
+    //WRITE NOW
+    spi_flash_write(flash_address, source_address, size);
+    
+    if(_esp8266_flash_debug)
+    {
+        os_printf("ESP8266 : FLASH : Flash write of %u bytes into address %x done\n", size, flash_address);
+    }
+    return;      
+}
+
+void ICACHE_FLASH_ATTR ESP8266_FLASH_ReadAddress(uint32_t flash_address, uint32_t* destination_address, uint32_t size)
+{
+    //READ THE SPECIFIED NUMBER OF BYTES FROM FLASH INTO THE DESTINATION ADDRESS
+    
+    //VERIFY FLASH ADDRESS
+    if(!_esp8266_flash_check_address_validity(flash_address))
+    {
+        if(_esp8266_flash_debug)
+        {
+            os_printf("ESP8266 : FLASH : Flash read. Starting flash address %x invalid\n", flash_address);
+        }
+        return;
+    }
+    
+    //READ
+    spi_flash_read(flash_address, destination_address, size);
+    
+    if(_esp8266_flash_debug)
+    {
+        os_printf("ESP8266 : FLASH : Flash read of %u bytes from address %x done\n", size, flash_address);
+    }
+    return;  
+}
+
+static uint8_t ICACHE_FLASH_ATTR _esp8266_flash_check_address_validity(uint32_t flash_address)
+{
+    //ENSURE THAT THE SPECIFIED FLASH ADDRESS IS WITHIN THE VALID RANGE
+    //AND DOES NOT LIE WITHIN PROTECTED / INVALID AREAS AS PER THE FLASH MAP ABOVE
+    
+    if(flash_address > ESP8266_FLASH_MAX_ADDRESS)
+    {
+        //OUT OF BOUND
+        return 0;
+    }
+    
+    if(flash_address >= ESP8266_FLASH_ESP_INIT_BIN_ADDRESS)
+    {
+        //IN PROTECTED AREA RESERVED FOR SYSTEM FILES
+        return 0;
+    }
+
+    if(flash_address <= ESP8266_FLASH_ESP_IROM0TEX_BIN_ADDRESS)
+    {
+        //FLASH ADDRESS WITHIN THE FIRST 64KNB AREA WHERE FLASH.BIN
+        //RESIDES. TECHNICALLY WE COULD USE IT DEPENDING ON SIDE OF FLASH.BIN
+        //BUT WE WILL DISALLOW IT
+        return 0
+    }
+    
+    //FLASH ADDRESS VALID
+    return 1;
 }
--- ESP8266_FLASH.h
+++ ESP8266_FLASH.h
@@ -2,31 +2,57 @@
 * ESP8266 FLASH LIBRARY
 *
 * NOVEMBER 2 2016
+*
 * ANKIT BHATNAGAR
 * ANKIT.BHATNAGARINDIA@GMAIL.COM
-* ***********************************************/
+*
+* REFERENCES
+*   (1) ESP8266 SDK GETTING STARTED GUIDE v2.8
+*       SECTION 4.1.1
+*       http://espressif.com/sites/default/files/documentation/2a-esp8266-sdk_getting_started_guide_en.pdf
+*   (2) ESP8266 FLASH RW OPERATIONS
+*       http://www.espressif.com/sites/default/files/99a-sdk-espressif_iot_flash_rw_operation_en_v1.0_0.pdf
+*
+* MOST OF THE 12F MODULES USE FLASH CHIP ID : 0x1640E0 (BERG MICRO 32MBIT / 4MB FLASH)
+*
+* FLASH IS A BLOCK DEVICE AND WORKS ON SECTORS (4KB)
+* BEFORE WRITING TO AN ADDRESS YOU NEED TO ERASE IT. ERASING CAN ONLY BE DONE AT THE LEAST OF A 4 KB BLOCK
+*
+* NOTE : FOR FLASH OPERATIONS, ALL READ/WRITE OPERATIONS NEED TO BE 4 BYTE ALIGNED
+*
+* AS PER THE DOCUMENTS, MEMORY MAP IS AS FOLLOWING (NON FOTA)
+*                                                                                                     0x400000
+* +------------+------------------------+-------------------------------------------+----------+------+
+* + FLASH      | IROM0TEXT              |       THIS AREA IS VARIABLE (DEPNDING     | ESP INIT | BLANK|
+* +  .BIN      |      .BIN              |        ON THE SIZE OF IROM0TEXT.BIN &     |     DATA |  .BIN|
+* + (MAX 64KB) |(MAX 76KB)              |        CAN BE USED TO STORE USER DATA     |    (8KB) | (8KB)|
+* +------------+------------------------+-------------------------------------------+----------+------+
+* 0x00000      0x10000                                                              0x3FC000   0x3FE000
+************************************************/
 
 #ifndef _ESP8266_FLASH_H_
 #define _ESP8266_FLASH_H_
 
-#include "user_interface.h"
+#include <stdint.h>
 #include <ets_sys.h>
 #include <spi_flash.h>
+#include "user_interface.h"
 
-//////////////////////////////////
-//DEFINES
-//////////////////////////////////
-//FLASH SECTOR (4KB) TO STORE PROGRAM DATA
-//ESP_INIT_DATA STARTS AT 0X3FC AS PER
-//http://www.espressif.com/en/support/explore/get-started/esp8266/getting-started-guide#
-/*http://www.esp8266.com/viewtopic.php?f=34&t=2662*/
-//STORE PROGRAM INFO 1 SECTOR BEFORE THAT
-//DO NOT CHANGE THIS ADDRESS !
-//VALID FOR 4M FLASH CHIP WITH 1024+1024 MAP
-#define FLASH_STORE_SECTOR  0x401
+#define ESP8266_FLASH_MAX_ADDRESS               0x400000
+#define ESP8266_FLASH_ESP_BLANK_BIN_ADDRESS     0x3FE000
+#define ESP8266_FLASH_ESP_INIT_BIN_ADDRESS      0x3FC000
+#define ESP8266_FLASH_ESP_IROM0TEX_BIN_ADDRESS  0x100000
 
-SpiFlashOpResult ESP8266_FLASH_erase(uint16_t sector_address);
-SpiFlashOpResult ESP8266_FLASH_write(uint32_t flash_address, uint32_t* source_address, uint32_t size);
-SpiFlashOpResult ESP8266_FLASH_read(uint32_t flash_address, uint32_t* destination_address, uint32_t size);
+//FUNCTION PROTOTYPES/////////////////////////////////////////////
+//CONFIGURATION FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_FLASH_SetDebug(uint8_t debug);
 
+//OPERATION FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_FLASH_EraseSector(uint16_t sector_number);
+void ICACHE_FLASH_ATTR ESP8266_FLASH_EraseBlock(uint16_t sector_number, uint16_t num_blocks);
+void ICACHE_FLASH_ATTR ESP8266_FLASH_WriteAddress(uint32_t flash_address, uint32_t* source_address, uint32_t size);
+void ICACHE_FLASH_ATTR ESP8266_FLASH_ReadAddress(uint32_t flash_address, uint32_t* destination_address, uint32_t size);
+
+//INTERNAL FUNCTIONS
+static uint8_t ICACHE_FLASH_ATTR _esp8266_flash_check_address_validity(uint32_t flash_address);
 #endif
